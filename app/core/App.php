9216e36c9f50a3da5ca571fdea45f6ff796ba18c
sakso <pawelgomolka@interia.pl>
2017-09-02 11:26:59 +0200

    Create auto loader class

--- app/core/App.php
+++ app/core/App.php
@@ -6,15 +6,17 @@ class App
     protected $controller = 'home';
     protected $method = 'index';
     protected $params = [];
+    private $loader;
 
     public function __construct()
     {
+        $this->loader = AutoLoader::getInstance(null);
+        $this->loader->changeRegister('loadController');
         $url = $this->parseUrl();
         if (file_exists('../app/controllers/' . $url[0] . '.php')) {
             $this->controller = $url[0];
             unset($url[0]);
         }
-        require_once '../app/controllers/' . $this->controller . '.php';
         $this->controller = new $this->controller;
         if (isset($url[1])) {
             if (method_exists($this->controller, $url[1])) {
@@ -23,10 +25,10 @@ class App
             } else {
                 $this->controller = "home";
                 $this->method = "error404";
-                require_once '../app/controllers/' . $this->controller . '.php';
                 $this->controller = new $this->controller;
             }
         }
+        $this->loader->changeRegister('loadModule');
         $this->params = $url ? array_values($url) : [];
         call_user_func_array([$this->controller, $this->method], $this->params);
     }
--- app/core/AutoLoader.php
+++ app/core/AutoLoader.php
@@ -8,8 +8,9 @@ class AutoLoader
     private $_lib;
     private $_models;
     private $_modules;
-    private $_view;
+    private $_custom;
     private $_controller;
+    private $_current;
 
     private function __construct($method)
     {
@@ -17,56 +18,54 @@ class AutoLoader
         $this->_lib = "../app/core/";
         $this->_modules = "../app/modules/built-in";
         $this->_models = "../app/models/";
-        $this->_view = "../app/view/";
         $this->_controller = "../app/controllers/";
-        spl_autoload_register(array($this, $method));
+        $this->_custom = "../app/modules/custom";
+        spl_autoload_extensions(".php");
+        $this->_current = array($this, $method);
+        spl_autoload_register($this->_current);
     }
 
     public function changeRegister($method)
     {
-        spl_autoload_register(array($this, $method));
+        spl_autoload_unregister($this->_current);
+        $this->_current = array($this, $method);
+        spl_autoload_register($this->_current);
     }
 
     public function loadLibrary($lib)
     {
-        spl_autoload_extensions(".php");
         set_include_path($this->_lib);
         return $this->_loadClass($lib);
     }
 
     public function loadModule($module)
     {
-        spl_autoload_extensions(".php");
         set_include_path($this->_modules);
         return $this->_loadClass($module);
     }
 
     public function loadModel($model)
     {
-        spl_autoload_extensions(".php");
         set_include_path($this->_models);
         return $this->_loadClass($model);
     }
 
     public function loadCore($core)
     {
-        spl_autoload_extensions(".php");
         set_include_path($this->_core);
         return $this->_loadClass($core);
     }
 
     public function loadController($controller)
     {
-        spl_autoload_extensions(".php");
         set_include_path($this->_controller);
         return $this->_loadClass($controller);
     }
 
-    public function loadView($view)
+    public function loadCustom($custom)
     {
-        spl_autoload_extensions(".php");
-        set_include_path($this->_modules);
-        return $this->_loadClass($view);
+        set_include_path($this->_custom);
+        return $this->_loadClass($custom);
     }
 
     private function _loadClass($class)
