d3681787550ceca8715dadc7a1a228a6f7a01693
sakso <pawelgomolka@interia.pl>
2018-03-15 23:38:53 +0100

    Router

--- app/lib/built/router/router.php
+++ app/lib/built/router/router.php
@@ -1,15 +1,46 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Lenovo
- * Date: 14.03.2018
- * Time: 00:52
- */
+
 
 namespace Lib\Built\Router;
 
+use Lib\Built\URI\URI;
 
+/**
+ * Standard class for routing.
+ * */
 class Router
 {
+    private $class;
+    private $method;
+    private $params;
 
+    public function __construct($class, $method, $params = false)
+    {
+        $this->method = $method;
+        $this->class = 'Controllers\\' . $class;
+        $this->params = $params;
+    }
+
+    public function renderRoute()
+    {
+        $uri = URI::getInstance();
+
+        $link = $uri->getBase();
+        $link .= '/' . $this->class;
+        $link .= '/' . $this->method;
+
+        if (!empty($this->params)) {
+            foreach ($this->params as $param) {
+                $link .= '/' . $param;
+            }
+        }
+
+        return $link;
+    }
+
+    public function execute()
+    {
+        $reflection = new \ReflectionMethod($this->class, $this->method);
+        $reflection->invoke(new $this->class, $this->params);
+    }
 }
